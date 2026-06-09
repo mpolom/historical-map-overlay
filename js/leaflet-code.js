e0b2a6c985b435010712e4d2eb712ae702ba2d69
Michael Polom <michaelpolom@gmail.com>
2026-06-09 21:17:43 +0100

    Improved commenting in leaflet code

--- js/leaflet-code.js
+++ js/leaflet-code.js
@@ -1,8 +1,7 @@
-
-
 import { Map, TileLayer, Marker, LatLngBounds, Control, Icon } from 'leaflet';
 import { GPX } from 'leaflet-gpx';
 
+// define tile layers for the map
 const histMap = new TileLayer('https://api.maptiler.com/tiles/uk-osgb10k1888/{z}/{x}/{y}.jpg?key=BnDgv89mXDjK9jv4TIw9', {
     attribution: '&copy; MapTiler',
     maxZoom: 17
@@ -11,8 +10,19 @@ const osmMap = new TileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png
     attribution: '&copy; OpenStreetMap contributors',
     maxZoom: 19
 });
+// Initialize the map and add the historical map layer
 const map = new Map('map');
 histMap.addTo(map);
+
+// Custom icon for the marker at the home of Edward Thomas, and add it to the map
+const MapIcon = new Icon({
+    iconUrl: './house-solid.svg',
+    iconSize: [18, 18],
+    iconAnchor: [18, 18]
+});
+new Marker([51.45994857367072, -0.16281375795570904], {title:'The home of Edward Thomas', icon: MapIcon}).addTo(map);
+
+// Load GPX files and fit the map to their bounds
 
 // URL to your GPX file or the GPX itself as a XML string.
 const gpxUrls = [
@@ -28,14 +38,6 @@ const options = {
     polyline_options: { color: 'green' }
 };
 
-const MapIcon = new Icon({
-    iconUrl: './house-solid.svg',
-    iconSize: [18, 18],
-    iconAnchor: [18, 18]
-});
-
-new Marker([51.45994857367072, -0.16281375795570904], {title:'The home of Edward Thomas', icon: MapIcon}).addTo(map);
-
 const allBounds = new LatLngBounds();
 let loadedCount = 0;
 
@@ -50,6 +52,7 @@ gpxUrls.forEach((url) => {
     }).addTo(map);
 });
 
+// Add layer control to switch between base layers
 const baseLayers = {
     "OSM": osmMap,
     "Historical": histMap
